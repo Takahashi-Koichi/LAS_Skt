d110e8bc41c66ed3c3115795df85ff8236f9cd55
Takahashi-Koichi <43104280+Takahashi-Koichi@users.noreply.github.com>
2019-01-23 16:37:39 +0900

    Add files via upload

---
+++ entire_text.xsl
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="html" encoding="utf-8" />
+<xsl:template match="/">
+	<html>
+		<body>
+    
+		<p>
+			<xsl:apply-templates select="TEI/body" />
+		</p>
+	</body>
+	</html>
+	
+</xsl:template>
+
+<xsl:template match="lb">
+	<sup><font size="-3" color="red"><xsl:value-of select="./@n" /></font></sup>
+</xsl:template>
+
+<xsl:template match="pb">
+	<sup><font size="-3" color="green">p.<xsl:value-of select="./@n" /></font></sup>
+</xsl:template>
+
+
+<xsl:template match="p">
+	<p><xsl:value-of select="." /></p>
+</xsl:template>
+
+</xsl:stylesheet>
---
+++ text_canon.js
@@ -0,0 +1,66 @@
+var xmlObj01;//楞伽経テキスト_TEI
+var xslObj01;//経典全体
+var xmlDoc01;
+var xslDoc01;
+
+var xslProc;
+var str="";
+var foundEntry;
+
+// ブラウザ判別IE11対応、2015/04/18
+//chang if(!document.all) into if(!isIE)
+var ua, isIE;
+
+// "http://garafu.blogspot.jp/2013/08/useragent-ie11.html"参照。
+// UserAgent を小文字に正規化
+ua = window.navigator.userAgent.toLowerCase();
+// IE or else 判別
+isIE = (ua.indexOf('msie') >= 0 || ua.indexOf('trident') >=0);
+
+function Init(){
+	//ブラウザ判別
+	if(!isIE){
+		// Mozilla
+		xmlObj01 = new XMLHttpRequest();
+		xmlObj01.open("GET","LAS.xml",false);
+		xmlObj01.setRequestHeader('Content-Type', 'text/xml');
+		xmlObj01.overrideMimeType('text/xml');
+		xmlObj01.send();
+		xmlDoc01 = xmlObj01.responseXML;
+
+		xslObj01 = new XMLHttpRequest();
+		xslObj01.open("GET","entire_text.xsl",false);
+		xslObj01.setRequestHeader('Content-Type', 'text/xml');
+		xslObj01.overrideMimeType('text/xml');
+		xslObj01.send();
+		xslDoc01 = xslObj01.responseXML;
+
+	}else{
+		//Internet Explorer
+		xmlDoc01=new ActiveXObject("MSXML2.DOMDocument");
+
+		xslDoc01=new ActiveXObject("MSXML2.FreeThreadedDOMDocument");
+		//*xml, *xslt読み込み
+		xmlDoc01.async=false;
+		xmlDoc01.load("LAS.xml");
+
+		xslDoc01.async=false;
+		xslDoc01.load("entire_text.xsl");
+	}
+//	ChangeImageGloss();
+}
+
+function CallText(){
+	if(!isIE){
+		// Mozilla
+		xslProc = new XSLTProcessor();
+		xslProc.importStylesheet(xslDoc01);
+		var fragment = xslProc.transformToFragment(xmlDoc01, document);
+		document.getElementById('dv').innerHTML = "";
+		document.getElementById('dv').appendChild(fragment);
+	}else{
+		//Internet Explorer
+		str = xmlDoc01.transformNode(xslDoc01);
+		dv.innerHTML = str;
+	}
+}
